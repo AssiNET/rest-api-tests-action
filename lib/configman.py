1b6572d3a0b4ba828e64ac0934f50413dfcfead9
Ivomir Assi <Assi.Ivomir@gmail.com>
2023-02-03 23:34:50 +0200

    Update configman.py

--- lib/configman.py
+++ lib/configman.py
@@ -21,8 +21,8 @@ class ConfigMan(object):
     CONTENT_DIR = os.path.join(ROOT_DIR, 'content')
     RESULTS_DIR = os.path.join(ROOT_DIR, 'results')
     LATEST_DIR = os.path.join(RESULTS_DIR, 'latest')
-    REPORT_HTML_FILE = os.path.join(ROOT_DIR, "Report.html")
-    REPORT_XML_FILE = os.path.join(ROOT_DIR, "Report.xml")
+    REPORT_HTML_FILE = os.path.join(ROOT_DIR, "report.html")
+    REPORT_XML_FILE = os.path.join(ROOT_DIR, "report.xml")
     LATEST_RESULTS_DIR = "DIR NOT SET"
     TEST_SUITE_NAME = ""
     CURRENT_TESTS_DIR = ""
